5365b8a4868b823edf044d747a399021a78a295b
aleksandrliubchich-sketch <aleksandrliubchich@gmail.com>
2025-11-26 17:12:37 +0000

    Update app.py

--- app.py
+++ app.py
@@ -249,13 +249,14 @@ def start_background_sweeper():
 
 # Start sweeper when app starts (works with Railway)
 @app.before_serving
-def on_start():
-    start_background_sweeper()
+def startup():
+    print("App started!")
 
 
 if __name__ == "__main__":
     # Local run
     start_background_sweeper()
     app.run(host="0.0.0.0", port=int(os.getenv("PORT", 5000)))
+
 
 
